27ed8548769e5384fdef999b23b023d6e9868e66
Farzaneh <farzanekazemi8517@Gmail.com>
2023-01-11 15:24:54 +0330

    Dynamic version

--- src/App.js
+++ src/App.js
@@ -3,20 +3,17 @@ import './App.css';
 import Modal from './modal/Modal';
 
 function App() {
+
+  const modalData = {
+    title: "Log in", 
+    massage: " Have you forget your Password?", 
+    postmassage: "Already have an account? Sign-in",
+    pass: "Password",
+    mail: "E-mail"
+  }
   return (
     
-      <Modal>
-      <div className="header">
-        <h2> Log in </h2>
-        <img src="./cancel.png" alt="Cancel"></img>
-        </div>
-        <div className='br'></div>
-        <input className="email" placeholder="E-mail" type="email"></input>
-        <input className="Password" placeholder="Password" type="password"></input>
-        <p className="pass-recovery"> Forget your password? </p>
-        <button className="login"> Login </button>
-        <div className='br'></div>
-        <p className="sign-in">Already have an account? Sign up </p>
+      <Modal {...modalData}>
     </Modal>
    
   );
--- src/modal/Modal.js
+++ src/modal/Modal.js
@@ -1,14 +1,31 @@
 import React from "react";
 import './Modal.css'
+import propTypes from 'prop-types'
+
+
 export default class Modal extends React.Component {
 
     render(){
 
         return (
             <div className="container">
-               {this.props.children}
+               <div className="header">
+                    <h2> {this.props.title} </h2>
+                    <img src="./cancel.png" alt="Cancel"></img>
+                    </div>
+                    <div className='br'></div>
+                    <input className="email" placeholder= {this.props.mail} type="email"></input>
+                    <input className="Password" placeholder= {this.props.pass} type="password"></input>
+                    <p className="pass-recovery"> {this.props.massage} </p>
+                    <button className="login"> {this.props.button} </button>
+                    <div className='br'></div>
+                    <p className="sign-in"> {this.props.postmassage} </p>
             </div>
         )
 
     }
+}
+
+Modal.propTypes = {
+    massage : propTypes.string.isRequired
 }
