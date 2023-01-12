049612078f82c4d9067c166d142ee0bba9ea152a
Farzaneh <farzanekazemi8517@Gmail.com>
2023-01-12 18:54:13 +0330

    input value handler

--- src/modal/Modal.js
+++ src/modal/Modal.js
@@ -4,7 +4,25 @@ import propTypes from 'prop-types'
 
 
 export default class Modal extends React.Component {
+    constructor (props){
+        super(props)
 
+        this.state = {
+            emailInputValue: "",
+            passwordInputValue: "",
+        }
+    }
+
+
+    emailChangeHandler(e){
+        console.log(e.target.value);
+       this.setState({emailInputValue: e.target.value}) 
+    }
+
+    passwordChangeHandler(e){
+        console.log(e.target.value);
+        this.setState({passwordInputValue: e.target.value})
+    }
     render(){
 
         return (
@@ -14,8 +32,8 @@ export default class Modal extends React.Component {
                     <img src="./cancel.png" alt="Cancel"></img>
                     </div>
                     <div className='br'></div>
-                    <input className="email" placeholder= {this.props.mail} type="email"></input>
-                    <input className="Password" placeholder= {this.props.pass} type="password"></input>
+                    <input className="email" placeholder= {this.props.mail} type="email" value={this.state.emailInputValue} onChange={e => this.emailChangeHandler(e)}></input>
+                    <input className="Password" placeholder= {this.props.pass} type="password" value={this.state.passwordInputValue} onChange={e => this.passwordChangeHandler(e)}></input>
                     <p className="pass-recovery"> {this.props.massage} </p>
                     <button className="login"> {this.props.button} </button>
                     <div className='br'></div>
