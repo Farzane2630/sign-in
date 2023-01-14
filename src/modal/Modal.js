2c73293a456ed740f6fcfd8da5cb51cbfeb2b90e
Farzaneh <farzanekazemi8517@Gmail.com>
2023-01-14 15:48:47 +0330

    v 3.0.0

--- src/modal/Modal.js
+++ src/modal/Modal.js
@@ -4,10 +4,7 @@ import propTypes from 'prop-types'
 
 
 export default class Modal extends React.Component {
-    constructor (props){
-        super(props)
-
-<<<<<<< HEAD
+    
     state ={
         emailValue: "",
         passwordValue: "",
@@ -19,24 +16,8 @@ export default class Modal extends React.Component {
     passwordChangeHandler (event){
         this.setState({passwordValue : event.target.value})
     }    
-=======
-        this.state = {
-            emailInputValue: "",
-            passwordInputValue: "",
-        }
-    }
 
 
-    emailChangeHandler(e){
-        console.log(e.target.value);
-       this.setState({emailInputValue: e.target.value}) 
-    }
-
-    passwordChangeHandler(e){
-        console.log(e.target.value);
-        this.setState({passwordInputValue: e.target.value})
-    }
->>>>>>> 049612078f82c4d9067c166d142ee0bba9ea152a
     render(){
 
         return (
@@ -46,13 +27,11 @@ export default class Modal extends React.Component {
                     <img src="./cancel.png" alt="Cancel"></img>
                     </div>
                     <div className='br'></div>
-<<<<<<< HEAD
+
                     <input className="email" placeholder= {this.props.mail} type="email" value={this.state.emailValue} onChange={(event)=> this.emailChangeHandler(event)}></input>
                     <input className="Password" placeholder= {this.props.pass} type="password" value={this.state.passwordValue} onChange={(event)=> this.passwordChangeHandler(event)}></input>
-=======
                     <input className="email" placeholder= {this.props.mail} type="email" value={this.state.emailInputValue} onChange={e => this.emailChangeHandler(e)}></input>
                     <input className="Password" placeholder= {this.props.pass} type="password" value={this.state.passwordInputValue} onChange={e => this.passwordChangeHandler(e)}></input>
->>>>>>> 049612078f82c4d9067c166d142ee0bba9ea152a
                     <p className="pass-recovery"> {this.props.massage} </p>
                     <button className="login"> {this.props.button} </button>
                     <div className='br'></div>
